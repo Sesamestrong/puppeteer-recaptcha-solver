ea13226bafedb3b23f2aabac0d5ada93c692c500
Sesamestrong <doolie.healey@gmail.com>
2020-03-04 19:10:29 -0500

    Make it actually good, still no repeat solves though

--- index.js
+++ index.js
@@ -1,92 +1,91 @@
 const axios = require('axios')
 const https = require('https')
 
-function rdn (min, max) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min)) + min
+function rdn(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min
 }
 
-async function solve (page) {
-  try {
-    await page.waitForFunction(() => {
-      const iframe = document.querySelector('iframe[src*="api2/anchor"]')
-      if (!iframe) return false
+async function solve(page) {
+    const getFrameByUrl = async (url, context = page) => {
+        await context.waitForSelector('iframe[src*="' + url + '"]');
+        return context.frames().find(frame => frame.url().includes(url));
+    };
+    try {
 
-      return !!iframe.contentWindow.document.querySelector('#recaptcha-anchor')
-    })
+        const recaptchaFrame = await getFrameByUrl("api2/anchor");
 
-    let frames = await page.frames()
-    const recaptchaFrame = frames.find(frame => frame.url().includes('api2/anchor'))
+        const checkbox = await recaptchaFrame.$('#recaptcha-anchor')
+        await checkbox.click({
+            delay: rdn(30, 150)
+        })
 
-    const checkbox = await recaptchaFrame.$('#recaptcha-anchor')
-    await checkbox.click({ delay: rdn(30, 150) })
+        const imageFrame = await getFrameByUrl("api2/bframe");
+        await imageFrame.waitForFunction(() => {
+            const img = document.querySelector('.rc-image-tile-wrapper img')
+            return img && img.complete;
+        });
 
-    await page.waitForFunction(() => {
-      const iframe = document.querySelector('iframe[src*="api2/bframe"]')
-      if (!iframe) return false
+        const audioButton = await imageFrame.$('#recaptcha-audio-button')
+        await audioButton.click({
+            delay: rdn(30, 150)
+        })
 
-      const img = iframe.contentWindow.document.querySelector('.rc-image-tile-wrapper img')
-      return img && img.complete
-    })
+        const httsAgent = new https.Agent({
+            rejectUnauthorized: false
+        })
 
-    frames = await page.frames()
-    const imageFrame = frames.find(frame => frame.url().includes('api2/bframe'))
-    const audioButton = await imageFrame.$('#recaptcha-audio-button')
-    await audioButton.click({ delay: rdn(30, 150) })
+        //TODO check if it gives us a mean error
+        const audioLinkSelector = '.rc-audiochallenge-tdownload-link';
+        await imageFrame.waitForSelector(audioLinkSelector);
+        const audioLink = (await imageFrame.$(audioLinkSelector)).href;
 
-    await page.waitForFunction(() => {
-      const iframe = document.querySelector('iframe[src*="api2/bframe"]')
-      if (!iframe) return false
 
-      return !!iframe.contentWindow.document.querySelector('.rc-audiochallenge-tdownload-link')
-    })
+        const audioBytes = await page.evaluate(audioLink => {
+            return (async () => {
+                const response = await window.fetch(audioLink)
+                const buffer = await response.arrayBuffer()
+                return Array.from(new Uint8Array(buffer))
+            })()
+        }, audioLink)
+        const response = await axios({
+            httsAgent,
+            method: 'post',
+            url: 'https://api.wit.ai/speech?v=20170307',
+            data: new Uint8Array(audioBytes).buffer,
+            headers: {
+                Authorization: 'Bearer JVHWCNWJLWLGN6MFALYLHAPKUFHMNTAC', //TODO get my own token
+                'Content-Type': 'audio/mpeg3'
+            }
+        })
 
-    const audioLink = await page.evaluate(() => {
-      const iframe = document.querySelector('iframe[src*="api2/bframe"]')
-      return iframe.contentWindow.document.querySelector('.rc-audiochallenge-tdownload-link').href
-    })
+        const audioTranscript = response.data._text.trim()
+        const input = await imageFrame.$('#audio-response')
+        await input.click({
+            delay: rdn(30, 150)
+        })
+        await input.type(audioTranscript, {
+            delay: rdn(30, 75)
+        })
 
-    const audioBytes = await page.evaluate(audioLink => {
-      return (async () => {
-        const response = await window.fetch(audioLink)
-        const buffer = await response.arrayBuffer()
-        return Array.from(new Uint8Array(buffer))
-      })()
-    }, audioLink)
+        const verifyButton = await imageFrame.$('#recaptcha-verify-button')
+        await verifyButton.click({
+            delay: rdn(30, 150)
+        })
 
-    const httsAgent = new https.Agent({ rejectUnauthorized: false })
-    const response = await axios({
-      httsAgent,
-      method: 'post',
-      url: 'https://api.wit.ai/speech?v=20170307',
-      data: new Uint8Array(audioBytes).buffer,
-      headers: {
-        Authorization: 'Bearer JVHWCNWJLWLGN6MFALYLHAPKUFHMNTAC',
-        'Content-Type': 'audio/mpeg3'
-      }
-    })
+        await page.waitForFunction(() => {
+            const iframe = document.querySelector('iframe[src*="api2/anchor"]')
+            if (!iframe) return false
 
-    const audioTranscript = response.data._text.trim()
-    const input = await imageFrame.$('#audio-response')
-    await input.click({ delay: rdn(30, 150) })
-    await input.type(audioTranscript, { delay: rdn(30, 75) })
+            return !!iframe.contentWindow.document.querySelector('#recaptcha-anchor[aria-checked="true"]')
+        })
 
-    const verifyButton = await imageFrame.$('#recaptcha-verify-button')
-    await verifyButton.click({ delay: rdn(30, 150) })
-
-    await page.waitForFunction(() => {
-      const iframe = document.querySelector('iframe[src*="api2/anchor"]')
-      if (!iframe) return false
-
-      return !!iframe.contentWindow.document.querySelector('#recaptcha-anchor[aria-checked="true"]')
-    })
-
-    return page.evaluate(() => document.getElementById('g-recaptcha-response').value)
-  } catch (e) {
-    console.log(e)
-    return ''
-  }
+        return page.evaluate(() => document.getElementById('g-recaptcha-response').value)
+    } catch (e) {
+        console.log(e)
+        return ''
+    }
 }
 
 module.exports = solve
