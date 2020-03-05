ce45807d0a3a0a09200b1bf957f8755fdd925d19
Sesamestrong <doolie.healey@gmail.com>
2020-03-04 20:24:15 -0500

    Make audio work, accept key and date

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const axios = require('axios')
 const https = require('https')
+const moment = require('moment');
 
 function rdn(min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min)) + min
-}
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+};
 
-async function solve(page) {
+async function solve(page, key, useOldModel) {
     const getFrameByUrl = async (url, context = page) => {
         await context.waitForSelector('iframe[src*="' + url + '"]');
         return context.frames().find(frame => frame.url().includes(url));
@@ -18,8 +19,8 @@ async function solve(page) {
 
         const checkbox = await recaptchaFrame.$('#recaptcha-anchor')
         await checkbox.click({
-            delay: rdn(30, 150)
-        })
+            delay: rdn(30, 150),
+        });
 
         const imageFrame = await getFrameByUrl("api2/bframe");
         await imageFrame.waitForFunction(() => {
@@ -29,63 +30,75 @@ async function solve(page) {
 
         const audioButton = await imageFrame.$('#recaptcha-audio-button')
         await audioButton.click({
-            delay: rdn(30, 150)
-        })
+            delay: rdn(30, 150),
+        });
 
         const httsAgent = new https.Agent({
             rejectUnauthorized: false
-        })
+        });
+        const apiOptions = {
+            httsAgent,
+            method: 'post',
+            url: 'https://api.wit.ai/speech?v=' + (useOldModel ? "20170304" : moment().subtract(2, "days").format('YYYYMMDD')),
+            headers: {
+                Authorization: `Bearer ${key}`,
+                'Content-Type': 'audio/mpeg3'
+            },
+        };
 
         //TODO check if it gives us a mean error
         const audioLinkSelector = '.rc-audiochallenge-tdownload-link';
         await imageFrame.waitForSelector(audioLinkSelector);
-        const audioLink = (await imageFrame.$(audioLinkSelector)).href;
+        /*const audioLink = await imageFrame.evaluate(() => {
+      return document.querySelector('.rc-audiochallenge-tdownload-link').href
+    })
+    */
+        const audioLink = await (await (await imageFrame.$(audioLinkSelector)).getProperty('href')).jsonValue();
 
-
-        const audioBytes = await page.evaluate(audioLink => {
+        const audioBytes = await imageFrame.evaluate(audioLink => {
             return (async () => {
-                const response = await window.fetch(audioLink)
-                const buffer = await response.arrayBuffer()
-                return Array.from(new Uint8Array(buffer))
-            })()
-        }, audioLink)
-        const response = await axios({
-            httsAgent,
-            method: 'post',
-            url: 'https://api.wit.ai/speech?v=20170307',
+                const response = await window.fetch(audioLink);
+                const buffer = await response.arrayBuffer();
+                return Array.from(new Uint8Array(buffer));
+            })();
+        }, audioLink);
+        const response = await axios({ ...apiOptions,
             data: new Uint8Array(audioBytes).buffer,
-            headers: {
-                Authorization: 'Bearer JVHWCNWJLWLGN6MFALYLHAPKUFHMNTAC', //TODO get my own token
-                'Content-Type': 'audio/mpeg3'
-            }
-        })
+        });
+        /*
+        const response = await axios({
+      httsAgent,
+      method: 'post',
+      url: 'https://api.wit.ai/speech?v=20170307',
+      data: new Uint8Array(audioBytes).buffer,
+      headers: {
+        Authorization: 'Bearer JVHWCNWJLWLGN6MFALYLHAPKUFHMNTAC',
+        'Content-Type': 'audio/mpeg3'
+      }
+    })
+            */
 
-        const audioTranscript = response.data._text.trim()
-        const input = await imageFrame.$('#audio-response')
+        const audioTranscript = response.data._text.trim();
+        const input = await imageFrame.$('#audio-response');
         await input.click({
-            delay: rdn(30, 150)
-        })
+            delay: rdn(30, 150),
+        });
         await input.type(audioTranscript, {
-            delay: rdn(30, 75)
-        })
+            delay: rdn(30, 75),
+        });
 
-        const verifyButton = await imageFrame.$('#recaptcha-verify-button')
+        const verifyButton = await imageFrame.$('#recaptcha-verify-button');
         await verifyButton.click({
-            delay: rdn(30, 150)
-        })
+            delay: rdn(30, 150),
+        });
 
-        await page.waitForFunction(() => {
-            const iframe = document.querySelector('iframe[src*="api2/anchor"]')
-            if (!iframe) return false
+        await recaptchaFrame.waitForSelector('#recaptcha-anchor[aria-checked="true"]');
 
-            return !!iframe.contentWindow.document.querySelector('#recaptcha-anchor[aria-checked="true"]')
-        })
-
-        return page.evaluate(() => document.getElementById('g-recaptcha-response').value)
+        return page.evaluate(() => document.getElementById('g-recaptcha-response').value);
     } catch (e) {
-        console.log(e)
-        return ''
+        console.log(e);
+        return '';
     }
-}
+};
 
-module.exports = solve
+module.exports = (key, useOldModel) => (page) => solve(page, key, useOldModel);
